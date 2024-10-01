fc37d8433a25f03359cdc9c8f691488d812b67de
tomasbabulak <babulak.tomas@gmail.com>
2024-10-01 17:00:54 +0200

    Configure firebase

--- SportTrack/AppDelegate.swift
+++ SportTrack/AppDelegate.swift
@@ -7,10 +7,12 @@
 
 import Foundation
 import SwiftUI
+import FirebaseCore
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
     return true
   }
 }
