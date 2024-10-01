b0b1477351e8029d40a5232145000f19106ff9b1
tomasbabulak <babulak.tomas@gmail.com>
2024-10-01 16:56:25 +0200

    App delegate

--- SportTrack/SportTrackApp.swift
+++ SportTrack/SportTrackApp.swift
@@ -10,23 +10,11 @@ import SwiftData
 
 @main
 struct SportTrackApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
+    @UIApplicationDelegateAdaptor private var delegate: AppDelegate
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView(viewModel: HomeViewModel())
         }
-        .modelContainer(sharedModelContainer)
     }
 }
