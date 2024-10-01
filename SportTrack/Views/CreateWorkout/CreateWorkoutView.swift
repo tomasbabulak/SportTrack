bb24cfb842fff6238a9933021676b15305a68933
tomasbabulak <babulak.tomas@gmail.com>
2024-10-01 16:55:15 +0200

    Create workout

--- SportTrack/Views/CreateWorkout/CreateWorkoutView.swift
+++ SportTrack/Views/CreateWorkout/CreateWorkoutView.swift
@@ -13,7 +13,75 @@ struct CreateWorkoutView: View {
 
     var body: some View {
         Form {
+            TextField("Enter Location", text: $viewModel.location)
+
+            durationPicker
+
+            storageTypePicker
         }
+        .toolbar {
+            ToolbarItem(placement: .confirmationAction) {
+                Button(action: {
+                    viewModel.saveTapped()
+                }, label: {
+                    Text("Save")
+                })
+            }
+
+            ToolbarItem(placement: .cancellationAction) {
+                Button(action: {
+                    viewModel.cancelTapped()
+                }, label: {
+                    Text("Cancel")
+                })
+            }
+        }
+        .navigationTitle("New Workout")
+        .alert(
+            "Missing Location",
+            isPresented: Binding($viewModel.destination.locationAlert),
+            actions: {
+                Button("Okay", role: .cancel) { }
+            },
+            message: {
+                Text("A workout must have a location.")
+            }
+        )
+    }
+
+    @ViewBuilder
+    private var durationPicker: some View {
+        VStack(alignment: .leading) {
+            Text("Enter Duration")
+                .font(.subheadline)
+
+            HStack {
+                Picker(selection: $viewModel.selectedHours, label: Text("Selected hours")) {
+                    ForEach(viewModel.hoursRange, id: \.self) { hour in
+                        Text("\(hour) h")
+                            .tag(hour)
+                    }
+                }
+                .pickerStyle(WheelPickerStyle())
+
+                Picker(selection: $viewModel.selectedMinutes, label: Text("Selected minutes")) {
+                    ForEach(viewModel.minutesRange, id: \.self) { minute in
+                        Text("\(minute) min")
+                            .tag(minute)
+                    }
+                }
+                .pickerStyle(WheelPickerStyle())
+            }
+        }
+    }
+
+    private var storageTypePicker: some View {
+        Toggle(
+            isOn: $viewModel.isCloud,
+            label: {
+                Text("Store in cloud")
+            }
+        )
     }
 }
 
--- SportTrack/Views/CreateWorkout/CreateWorkoutViewModel.swift
+++ SportTrack/Views/CreateWorkout/CreateWorkoutViewModel.swift
@@ -6,17 +6,61 @@
 //
 
 import SwiftUI
+import SwiftUINavigation
+import Dependencies
 
 @Observable
 final class CreateWorkoutViewModel: Identifiable {
+    let id: UUID = UUID()
+    @ObservationIgnored
+    @Dependency(\.date.now) var now
+    @ObservationIgnored
+    @Dependency(\.uuid) var uuid
+
+    var destination: Destination?
+
+    @CasePathable
+    enum Destination {
+      case locationAlert
+    }
+
     enum WorkoutResultHandler {
         case cancelled
-        case created(Workout, StorageType)
+        case created(Workout)
     }
 
     let resultHandler: (WorkoutResultHandler) -> Void
 
+    var location: String = ""
+    var selectedHours: Int = 0
+    var selectedMinutes: Int = 0
+    var isCloud: Bool = false
+
+    let hoursRange: [Int] = Array(0...23)
+    let minutesRange: [Int] = Array(0...59)
+
     init(resultHandler: @escaping (WorkoutResultHandler) -> Void) {
         self.resultHandler = resultHandler
+    }
+
+    func saveTapped() {
+        guard
+            !location.isEmpty
+        else {
+            destination = .locationAlert
+            return
+        }
+        let workout = Workout(
+            id: uuid(),
+            timestamp: now,
+            location: location,
+            duration: .seconds(selectedHours * 3600 + selectedMinutes * 60),
+            storage: isCloud ? .cloud : .local
+        )
+        resultHandler(.created(workout))
+    }
+
+    func cancelTapped() {
+        resultHandler(.cancelled)
     }
 }
