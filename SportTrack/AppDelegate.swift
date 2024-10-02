fbb397e3f30ff58dd7898729890fb374bf3052f1
tomasbabulak <babulak.tomas@gmail.com>
2024-10-02 10:42:37 +0200

    Formatting

--- SportTrack/AppDelegate.swift
+++ SportTrack/AppDelegate.swift
@@ -10,9 +10,11 @@ import SwiftUI
 import FirebaseCore
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
 }
